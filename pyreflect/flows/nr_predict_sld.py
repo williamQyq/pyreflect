fix bug SLD prediction normalization process

pyreflect/flows/nr_predict_sld.py
@@ -20,8 +20,7 @@ def generate_nr_sld_curves(params:NRSLDCurvesGeneratorParams)->Tuple[np.ndarray,
         dir (str): Directory where the files will be saved.
 
     """
-    phy_film_model = ReflectivityModel(params.num_film_layers)
-    m = ReflectivityDataGenerator(model=phy_film_model)
+    m = ReflectivityDataGenerator(model=ReflectivityModel(params.num_film_layers))
     processed_nr, processed_sld_profile = m.generate_curves(params.num_curves)
 
     np.save(params.mod_sld_file, processed_sld_profile)
@@ -47,11 +46,13 @@ def train_nr_predict_sld_model(params:NRSLDModelTrainerParams, auto_save=True)->
     :raise FileNotFoundError:
     """
     data_processor = NRSLDDataProcessor(params.nr_file,params.sld_file)
+    data_processor.load_data()
 
+    # NR-SLD curves are already normalized during generation
     trainer = NRSLDModelTrainer(
         data_processor=data_processor,
-        X=data_processor.normalize_nr(),
-        y=data_processor.normalize_sld(),
+        X=data_processor._nr_arr,
+        y=data_processor._sld_arr,
         layers=params.layers,
         batch_size=params.batch_size,
         epochs=params.epochs,
@@ -80,10 +81,9 @@ def predict_sld_from_nr(model, nr_file:str | Path)->np.ndarray:
             np.ndarray: Predicted SLD curves.
     """
     try:
+        # Load data
         processor = NRSLDDataProcessor(nr_file_path=nr_file)
-        # load data into nr_arr
         processor.load_data()
-
     except FileNotFoundError as e:
         typer.echo(e)
         raise typer.Exit()
@@ -91,18 +91,13 @@ def predict_sld_from_nr(model, nr_file:str | Path)->np.ndarray:
     # Normalization
     normalized_nr_arr = processor.normalize_nr()
 
-    typer.echo(f"Processed NR shape:{normalized_nr_arr.shape}\n")
-
     #Remove wave vector (x channel) of NR
     reshaped_nr_curves = processor.reshape_nr_to_single_channel(normalized_nr_arr)
-
-    # Stack all curves into a batch for efficient model inference
-    reshaped_nr_curves = np.stack(reshaped_nr_curves)
 
     #Prediction
     predicted_sld_curves = _predict(model, reshaped_nr_curves)
 
-    typer.echo(f"Prediction SLD shape: {predicted_sld_curves.shape}")
+    typer.echo(f"Predicted SLD shape: {predicted_sld_curves.shape}")
 
     return predicted_sld_curves
 

pyreflect/input/data_processor.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
@@ -135,12 +135,32 @@ class NRSLDDataProcessor(DataProcessor):
         super().__init__(seed)
         self.nr_file_path = nr_file_path
         self.sld_file_path = sld_file_path
-        self._nr_arr = np.load(self.nr_file_path) if nr_file_path else None
-        self._sld_arr = np.load(self.sld_file_path) if sld_file_path else None
+        self._nr_arr = None
+        self._sld_arr = None
+
+    def load_data(self,new_nr_file=None, new_sld_file=None):
+        """
+
+        :param new_nr_file:
+        :param new_sld_file:
+        :return:
+
+        :raise
+        - OSError
+        """
+        # Decide file paths: use new ones if provided, else use those from init
+        nr_path = new_nr_file if new_nr_file is not None else self.nr_file_path
+        sld_path = new_sld_file if new_sld_file is not None else self.sld_file_path
+
+        if nr_path:
+            self._nr_arr = np.load(nr_path)
+
+        if sld_path:
+            self._sld_arr = np.load(sld_path)
 
     def normalize_nr(self):
         """Normalizes NR curves."""
-        if not self._nr_arr:
+        if self._nr_arr is None:
             raise FileNotFoundError(f"NR file not loaded from path:{self.nr_file_path}")
 
         # Reflectivity decreases exponentially, log transformation compress large range
@@ -148,7 +168,7 @@ class NRSLDDataProcessor(DataProcessor):
 
     def normalize_sld(self):
         """Normalizes SLD curves."""
-        if not self._sld_arr:
+        if self._sld_arr is None:
             raise FileNotFoundError(f"SLD File not loaded from path: {self.sld_file_path}")
 
         return DataProcessor.normalize_xy_curves(self._sld_arr,scale=(0,1),apply_log=True)

pyreflect/input/reflectivity_data_generator.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 import typer
-from pyreflect.input import NRSLDDataProcessor
+from pyreflect.input import NRSLDDataProcessor, DataProcessor
 from scipy.interpolate import CubicSpline
 from refl1d.names import QProbe,Slab,SLD,Parameter,Experiment
 
@@ -143,6 +143,15 @@ class ReflectivityModel:
 
         return normalized_nr
 
+    @property
+    def refl_array(self):
+        return self._refl_array
+
+    @property
+    def smooth_array(self):
+        return self._smooth_array
+
+
 class ReflectivityDataGenerator:
     def __init__(self, model:ReflectivityModel):
         #Simulated physical model
@@ -159,30 +168,17 @@ class ReflectivityDataGenerator:
         self._train_pars = np.random.uniform(low=-1, high=1, size=[n, len(self.model.parameters)])
         self.model.compute_reflectivity(self._train_pars)
 
-        norm_nr = self.model.preprocess_nr(noise=None)
+        if self.model.refl_array is None:
+            raise ValueError("No reflectivity data generated")
 
-        processed_nr, processed_sld = self.process_curves(norm_nr,self.model._smooth_array)
+        norm_nr = self.model.preprocess_nr()
+        norm_sld = DataProcessor.normalize_xy_curves(self.model.smooth_array, scale=(0, 1))
 
-        print(f"processed NR shape:{processed_nr.shape}\n")
-        print(f"processed SLD shape:{processed_sld.shape}")
+        refl_arr = np.stack([np.array([self.model.q, refl]) for refl in norm_nr])
+        print(f"processed NR shape:{refl_arr.shape}\n")
+        print(f"processed SLD shape:{norm_sld.shape}")
 
-        return processed_nr, processed_sld
-
-    def process_curves(self,nr_arr, sld_profiles:List):
-
-        # Normalize SLD profiles
-        processed_sld = NRSLDDataProcessor.normalize(sld_profiles)
-
-        q_values = self.model.q
-        refl_curves = nr_arr
-
-        # Stack reflectivity data with q-values
-        refl_arr = np.stack([np.array([q_values, refl]) for refl in refl_curves])
-
-        typer.echo(f"processed NR shape:{refl_arr.shape}\n")
-        typer.echo(f"processed SLD shape:{processed_sld.shape}")
-
-        return refl_arr, processed_sld
+        return refl_arr, norm_sld
 
     def save(self, output_dir=''):
         np.save(os.path.join(output_dir, f"{self._config_name}_q_values"), self.q)