750b01d2304957760793d32f7e6f53fffc0600fe
NESSIE\qyqfi <qyqfiles@hotmail.com>
2025-12-23 23:51:19 -0500

    [Notebook] example reflectivity pipeline notebook

--- datasets/readme.md
+++
@@ -1,2 +0,0 @@
-- `combined_nr.npy` and `combined_sld.npy` are the input **X** and **y** for the training dataset, generated using `NRSLDDataGenerator`.
-- `combined_expt_denoised_nr.npy` contains the experimental data collected from the lab, comprising a total of 8 datasets.
--- src/pyreflect/pipelines/__init__.py
+++ src/pyreflect/pipelines/__init__.py
@@ -3,9 +3,10 @@ from . import train_autoencoder_mlp_chi_pred as chi_prediction_model_trainer
 # Selective function imports for direct access
 from .run_chi_prediction import run_chi_prediction
 from .run_sld_prediction import run_sld_prediction
+from .helper import compute_nr_from_sld
 __all__ = [
+    "compute_nr_from_sld",
     "chi_prediction_model_trainer",  # Full module alias
-    "sld_profile_pred",  # Full module alias
     "reflectivity_pipeline",  # Full module alias
     "run_chi_prediction",  # Direct function
     "run_sld_prediction",  # Direct function
